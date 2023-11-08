91790c1680ba2f342386f2c7205500cbe886eaa5
Aviv <97045970+AvivBenRubi1@users.noreply.github.com>
2023-11-08 16:51:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,44 +6,58 @@ const DroneData = require("./DTOs/drone-data.dto");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
-let drone_id = 1052654852;
 
 io.on("connection", (socket) => {
-  console.log("A user connected");
-  socket.on("dji_telemetry", (sd) => {
-    console.log(sd);
-    socket.broadcast.emit("dji_telemetry", sd);
-  });
+  function startSimulation() {
+    drone_id = 1;
 
-  const coordinates = [
-    [31.9560679241175, 34.842889310330996],
-    [31.957824237325653, 34.83903162954704],
-    [31.95471104357846, 34.839407138809634],
-  ];
-  const a = coordinates.map((coord) => coord.reverse());
+    // function createDrone() {
+    //   let droneData = new DroneData(31.5017, 34.4668, drone_id.toString());
+    //   const interval = setInterval(() => {
+    //     socket.emit("dji_telemetry", droneData);
+    //     droneData.latitude += 0.004;
+    //     droneData.longitude += 0.003;
+    //   }, 1000);
+    //   drone_id++;
+    // }
 
-  socket.emit("get_antenna", {
-    latitude: 31.956506679241175,
-    longitude: 34.842889310330996,
-    geo: a
-  });
+    // createDrone();
 
-  
-  function createDrone() {
-    let droneData = new DroneData(31.5017, 34.4668, drone_id.toString());
-    console.log(droneData, "start");
-    const interval = setInterval(() => {
-      socket.emit("dji_telemetry", droneData);
-      droneData.latitude += 0.024;
-      droneData.longitude += 0.023;
-      droneData.altitude = 123.254;
-    }, 1000);
-    if (drone_id < 1052654854) drone_id++;
+    const coordinates = [
+      [31.95650679241175, 34.842889310330996],
+      [31.957824237325653, 34.83903162954704],
+      [31.95471104357846, 34.839407138809634]
+    ]
+
+    const a = coordinates.map(coord => coord.reverse())
+    socket.emit("get_antenna", {
+      latitude: 31.95650679241175,
+      longitude: 34.842889310330996,
+      geo: a
+    });
+
+    var interval = setInterval(() => {
+      //createDrone();
+    }, 20000);
   }
 
-  var interval = setInterval(() => {
-    createDrone();
-  }, 20000);
+  console.log("A user connected");
+  startSimulation();
+
+  socket.on("dji_telemetry", (sd) => {
+    console.log(sd)
+    socket.broadcast.emit("dji_telemetry", sd)
+  });
+
+  socket.on("drone_exist", (sd) => {
+    console.log(sd)
+    socket.broadcast.emit("dji_telemetry", sd)
+  });
+
+  socket.on("power_check_packet", (sd) => {
+    console.log(sd)
+    socket.broadcast.emit("dji_telemetry", sd)
+  });
 
   socket.on("disconnect", () => {
     console.log("A user disconnected\n");
@@ -52,6 +66,6 @@ io.on("connection", (socket) => {
 
 const PORT = 4000;
 
-server.listen(PORT, "0.0.0.0", () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
